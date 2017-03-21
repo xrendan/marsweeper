5807bc69878104b7c5a3f391b0b2ba4358b608b4
Brendan Samek <samek@ualberta.ca>
2017-03-21 13:39:52 -0600

    Change version to 0.0.2

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name = 'marsweeper',
     packages = ['marsweeper'], # this must be the same as the name above
-    version = '0.0.1',
+    version = '0.0.2',
     description = 'minesweeper for martians',
     author = 'Brendan Samek and Tristan Ringland',
     author_email = 'xrendan@gmail.com',
     url = 'https://github.com/xrendan/marsweeper',
-    download_url = 'https://github.com/xrendan/marsweeper/archive/0.0.1.tar.gz',
+    download_url = 'https://github.com/xrendan/marsweeper/archive/0.0.2.tar.gz',
     keywords = ['minesweeper', 'mars', 'CMPUT275'], # arbitrary keywords
     classifiers = [],
 )
