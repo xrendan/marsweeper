303c1d9fb8241ca90da14ad240d461c8e2e3d531
Brendan Samek <samek@ualberta.ca>
2017-03-21 13:34:01 -0600

    Add setup and config files for publishing on PyPi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'marsweeper',
+    packages = ['marsweeper'], # this must be the same as the name above
+    version = '0.0.1',
+    description = 'minesweeper for martians',
+    author = 'Brendan Samek and Tristan Ringland',
+    author_email = 'xrendan@gmail.com',
+    url = 'https://github.com/xrendan/marsweeper',
+    download_url = 'https://github.com/xrendan/marsweeper/archive/0.0.1.tar.gz',
+    keywords = ['minesweeper', 'mars', 'CMPUT275'], # arbitrary keywords
+    classifiers = [],
+)
